5cbd963eca18765733640153fa8e92efa873b321
Roy <roy@haroush.net>
2016-01-03 03:09:51 +0200

    fixed gnmap parser

--- gnmap-parser.sh
+++ gnmap-parser.sh
@@ -1,9 +1,25 @@
- clear
+#!/bin/bash
+
+# Global Variables
+parsedir=Parsed-Results
+portldir=${parsedir}/Port-Lists
+portfdir=${parsedir}/Port-Files
+portmdir=${parsedir}/Port-Matrix
+hostldir=${parsedir}/Host-Lists
+hosttype=${parsedir}/Host-Type
+thrdprty=${parsedir}/Third-Party
+ipsorter='sort -n -u -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4'
+whatport='more /usr/local/share/nmap/nmap-services |  tr -s "/" " "  | tr '\t' ' ' | cut -d" " -f1,2,3 | grep -i tcp | grep -w $1'
+
+# Title Function
+func_title(){
+  clear
   echo '============================================================================'
   echo ' Gnmap-Parser.sh | [Version]: 3.4.2 | [Updated]: 05.01.2014'
   echo '============================================================================'
   echo
 }
+
 # Gather Gnmap Files Function
 func_gather(){
   # Validation Checks For Path Arguments
@@ -36,6 +52,7 @@ func_gather(){
     fi
   fi
 }
+
 # Heuristically Gather Gnmap Files Function
 func_heuristic(){
   # Local Variables
@@ -74,6 +91,7 @@ func_heuristic(){
     fi
   fi
 }
+
 # Function To Parse .gnmap Files
 func_parse(){
   # Check For .gnmap Files Before Parsing
@@ -89,6 +107,7 @@ func_parse(){
     echo
     exit 1
   fi
+
   # Create Parsing Directories If Non-Existent
   echo '[*] Preparing Directories'
   for dir in ${parsedir} ${portldir} ${portfdir} ${portmdir} ${hostldir} ${hosttype} ${thrdprty}; do
@@ -96,11 +115,13 @@ func_parse(){
         mkdir ${dir}
     fi
   done
+
   # Build Alive Hosts Lists
   func_title
   echo '[*] Building Alive Hosts Lists'
   awk '!/^#|Status: Down/' *.gnmap|sed -e 's/Host: //g' -e 's/ (.*//g'|${ipsorter} > ${hostldir}/Alive-Hosts-ICMP.txt
   awk '!/^#/' *.gnmap|grep "open/"|sed -e 's/Host: //g' -e 's/ (.*//g'|${ipsorter} > ${hostldir}/Alive-Hosts-Open-Ports.txt
+
   # Build Host-Type Lists
   func_title
   echo '[*] Building Host-Type Windows List'
@@ -108,33 +129,40 @@ func_parse(){
   WINRULE02=$(grep "135/open/tcp" *.gnmap|grep -v "445/open/tcp"| cut -d" " -f2)
   WINRULE03=$(grep "445/open/tcp" *.gnmap|grep "3389/open/tcp"|cut -d" " -f2)
   echo ${WINRULE01} ${WINRULE02} ${WINRULE03}|tr ' ' '\n'|${ipsorter} > ${hosttype}/Windows.txt
+
   func_title
   echo '[*] Building Host-Type UNIX/Linux List'
   NIXRULE01=$(grep "22/open/tcp" *.gnmap|grep -v "23/open/tcp"|cut -d" " -f2)
   NIXRULE02=$(grep "111/open/tcp" *.gnmap|grep -v "445/open/tcp"|cut -d" " -f2)
   echo ${NIXRULE01} ${NIXRULE02}|tr ' ' '\n'|${ipsorter} > ${hosttype}/Nix.txt
+
   func_title
   echo '[*] Building Host-Type Webservers List'
   WEBRULE01=$(grep "80/open/tcp" *.gnmap|cut -d" " -f2)
   WEBRULE02=$(grep "443/open/tcp" *.gnmap|cut -d" " -f2)
   echo ${WEBRULE01} ${WEBRULE02}|tr ' ' '\n'|${ipsorter} > ${hosttype}/Webservers.txt
+
   func_title
   echo '[*] Building Host-Type Network Devices List'
   NETRULE01=$(grep "80/open/tcp" *.gnmap|grep "23/open/tcp"|grep "22/open/tcp"|grep -v "445/open/tcp"|cut -d" " -f2)
   echo ${NETRULE01}|tr ' ' '\n'|${ipsorter} > ${hosttype}/Network-Devices.txt
+
   func_title
   echo '[*] Building Host-Type Printers List'
   PRNRULE01=$(grep "80/open/tcp" *.gnmap|grep "23/open/tcp"|grep "22/open/tcp"|grep "445/open/tcp"|cut -d" " -f2)
   PRNRULE02=$(grep "1900/open/tcp" *.gnmap|cut -d" " -f2)
   echo ${PRNRULE01}|tr ' ' '\n'|${ipsorter} > ${hosttype}/Printers.txt
+
   # Build TCP Ports List
   func_title
   echo '[*] Building TCP Ports List'
   grep "Ports:" *.gnmap|sed -e 's/^.*Ports: //g' -e 's;/, ;\n;g'|awk '!/udp/'|grep 'open'|cut -d"/" -f 1|sort -n -u > ${portldir}/TCP-Ports-List.txt
+
   # Build UDP Ports List
   func_title
   echo '[*] Building UDP Ports List'
   grep "Ports:" *.gnmap|sed -e 's/^.*Ports: //g' -e 's;/, ;\n;g'|awk '!/tcp/'|grep 'open'|cut -d"/" -f 1|sort -n -u > ${portldir}/UDP-Ports-List.txt
+
   # Build TCP Port Files
   for port in $(cat ${portldir}/TCP-Ports-List.txt); do
     TCPPORT="${port}"
@@ -144,6 +172,7 @@ func_parse(){
     WHAT=$(more /usr/share/nmap/nmap-services |  tr -s "/" " "  | tr '\t' ' ' | cut -d" " -f1,2,3 | grep -i tcp | grep -w $TCPPORT | cut -d" " -f1)
     cat *.gnmap|grep " ${TCPPORT}/open/tcp"|sed -e 's/Host: //g' -e 's/ (.*//g'|${ipsorter} > ${portfdir}/${TCPPORT}-"$WHAT"-TCP.txt
   done
+
   # Build UDP Port Files
   for port in $(cat ${portldir}/UDP-Ports-List.txt); do
     UDPPORT="${port}"
@@ -152,6 +181,7 @@ func_parse(){
     echo "The Current UDP Port Is: ${UDPPORT}"
     cat *.gnmap|grep " ${UDPPORT}/open/udp"|sed -e 's/Host: //g' -e 's/ (.*//g'|${ipsorter} > ${portfdir}/${UDPPORT}-UDP.txt
   done
+
   # Remove Stale Matrices
   for p in TCP UDP; do
     if [ -f ${portmdir}/${p}-Services-Matrix.csv ]; then
@@ -160,6 +190,7 @@ func_parse(){
       rm ${portmdir}/${p}-Services-Matrix.csv
     fi
   done
+
   # Build TCP Services Matrix
   for port in $(cat ${portldir}/TCP-Ports-List.txt); do
     TCPPORT="${port}"
@@ -168,6 +199,7 @@ func_parse(){
     echo "The Current TCP Port Is: ${TCPPORT}"
     cat *.gnmap|grep " ${port}/open/tcp"|sed -e 's/Host: //g' -e 's/ (.*//g' -e "s/$/,TCP,${port}/g"|${ipsorter} >> ${portmdir}/TCP-Services-Matrix.csv
   done
+
   # Build UDP Services Matrix
   for port in $(cat ${portldir}/UDP-Ports-List.txt); do
     UDPPORT="${port}"
@@ -176,12 +208,14 @@ func_parse(){
     echo "The Current UDP Port Is: ${UDPPORT}"
     cat *.gnmap|grep " ${port}/open/udp"|sed -e 's/Host: //g' -e 's/ (.*//g' -e "s/$/,UDP,${port}/g"|${ipsorter} >> ${portmdir}/UDP-Services-Matrix.csv
   done
+
   # Remove Stale PeepingTom File
   if [ -f ${thrdprty}/PeepingTom.txt ]; then
     func_title
     echo '[*] Removing Stale PeepingTom.txt'
     rm ${thrdprty}/PeepingTom.txt
   fi
+
   # Build PeepingTom Input File
   for i in $(cat ${portldir}/TCP-Ports-List.txt); do
     TCPPORT="${i}"
@@ -193,15 +227,18 @@ func_parse(){
     cat *.gnmap|grep " ${i}/open/tcp//https/\| ${i}/open/tcp//https-alt/\| ${i}/open/tcp//https?/\| ${i}/open/tcp//ssl|http/"|\
          sed -e 's/Host: //g' -e 's/ (.*//g' -e "s.^.https://.g" -e "s/$/:${i}/g"|${ipsorter} >> ${thrdprty}/PeepingTom.txt
   done
+
   # Remove Empty Files
   func_title
   echo '[*] Removing Empty Files'
   find ${parsedir} -size 0b -exec rm {} \;
   find ${parsedir} -size 1c -exec rm {} \;
+
   # Show Complete Message
   func_title
   echo '[*] Parsing Complete'
 }
+
 # Start Statement
 func_title
 case ${1} in
@@ -222,5 +259,7 @@ case ${1} in
     echo '           -p  = Parse .gnmap Files'
     echo '           -h  = Show This Help'
 esac
+
 # End Spacer
 echo
+
