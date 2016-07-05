35982f48f2812fcdbe1da49bf614a359f690478a
royharoush <roy@haroush.net>
2016-07-05 14:11:14 +0300

    Update nmap-Download-Parse.sh

--- nmap-Download-Parse.sh
+++ nmap-Download-Parse.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 echo    "########## Downloading GNX and gnmap-parser tools to this directory ############"
 wget https://bitbucket.org/memoryresident/gnxtools/raw/fde3449ff2756686e001ac4f7a45849a187f3710/gnxparse.py &> /dev/null
 wget https://bitbucket.org/memoryresident/gnxtools/raw/fde3449ff2756686e001ac4f7a45849a187f3710/gnxmerge.py &> /dev/null
 wget https://raw.githubusercontent.com/royharoush/rtools/master/gnmap-parser.sh &> /dev/null
 now=$(date +"%d-%m-%y"-"%T" |tr ":" "-" | cut -d"-" -f1,2,3,4,5)
+mkdir Results-$now
 echo    "########## Download Complete ############"
 echo    "########## GNX Nmap Tools Are Now Inside Your Directory ############"
 echo    "########## Modified Gnmap-Parser is now Inside Your Directory ############"
@@ -11,16 +13,15 @@ echo #find -name '*.xml'   | xargs -I{} grep -LZ "state=\"open\"" {} | while IFS
 echo "I will now parse all your XMLs into one file called gnx-merged-$now.xml" 
 python gnxmerge.py -s ./  > gnx-merged-$now.xml
 echo "I will now create the outputs of your scans from the XML file" 
-python gnxparse.py gnx-merged.xml -i -p -s -r -c >> gnx-output_all-$now.csv 
-python gnxparse.py gnx-merged.xml -p >> gnx-Open-Ports-$now.txt 
-python gnxparse.py gnx-merged.xml -i >> gnx-Live-IPs-$now.txt
-python gnxparse.py gnx-merged.xml -s >> gnx-Subnets-$now.txt 
-python gnxparse.py gnx-merged.xml -c >> gnx-Host-Ports-Matrix.csv  
-python gnxparse.py gnx-merged.xml -r 'nmap -A' >> ./gnx-suggested_scans-$now.sh
-echo "########All Done, Merged XML is in gnx-merged.xml########"
+python gnxparse.py gnx-merged-$now.xml -i -p -s -r -c >> gnx-output_all-$now.csv 
+python gnxparse.py gnx-merged-$now.xml -p >> gnx-Open-Ports.txt 
+python gnxparse.py gnx-merged-$now.xml -i >> gnx-Live-IPs.txt
+python gnxparse.py gnx-merged-$now.xml -s >> gnx-Subnets.txt 
+python gnxparse.py gnx-merged-$now.xml -c >> gnx-Host-Ports-Matrix.csv  
+python gnxparse.py gnx-merged-$now.xml -r 'nmap -A' >> ./gnx-suggested_scans-$now.sh
+echo "########All Done, Merged XML is in gnx-merged-$now.xml########"
 echo "########Scan data can be found in gnx* files########" 
 echo "############parsing Gnmap files##########"
-mkdir Results-$now
 find . -maxdepth 1 -type f -name '*.gnmap' -print0 |  sort -z |  xargs -0 cat -- >> ./Results-$now/gnmap-merged-$now.gnmap
 echo "############parsing Gnmap files##########"
 mv gnmap-parser.sh ./Results-$now
