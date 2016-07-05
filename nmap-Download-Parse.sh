b8387e12c158c770ba38d25aca49c0f4236b25ea
royharoush <roy@haroush.net>
2016-07-05 14:03:23 +0300

    Update nmap-Download-Parse.sh

--- nmap-Download-Parse.sh
+++ nmap-Download-Parse.sh
@@ -28,8 +28,8 @@ cd Results-$now
 bash gnmap-parser.sh -p
 mv ../gnx* ./
 #cd Results-$now
-cat ./Parsed-Results/Host-Lists/Alive-Hosts-Open-Ports.txt > Gnmap-$now-LiveHosts.txt
-cat ./Parsed-Results/Port-Lists/TCP-Ports-List.txt  | tr "\n" "," > Gnmap-$now-OpenPorts.txt
+cat .Results-$now/Parsed-Results/Host-Lists/Alive-Hosts-Open-Ports.txt > Gnmap-$now-LiveHosts.txt
+cat .Results-$now/Parsed-Results/Port-Lists/TCP-Ports-List.txt  | tr "\n" "," > Gnmap-$now-OpenPorts.txt
 echo "#### Downloading nmapParse.sh####"
 #https://raw.githubusercontent.com/royharoush/rtools/master/nmapParse.sh &> /dev/null
 #echo "#### To parse again run 'bash nmapParse.sh' ####"
