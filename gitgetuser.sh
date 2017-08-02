568ad92c5c93deb97b0027838873e9a4135b9ed8
royharoush <roy@haroush.net>
2017-08-02 18:19:26 +0300

    Update gitgetuser.sh

--- gitgetuser.sh
+++ gitgetuser.sh
@@ -4,7 +4,7 @@ if [ "$#" -ne 2 ]; then
     echo "exmaple: gitgetuser.sh royharoush starred\repos"
 	else 
 	echo "Getting the $2 for $1"
-	for page in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ; do curl "https://api.github.com/users/$1/$2?page=$page&per_page=99" | grep  'name\|"description\|clone_url' | sed s'\,\\' | sed "/\b\(labels_url\|full_name\)\b/d"  |tr -d '"' |  sed s'jname:j##############j' | sed s'kdescription:k###k'| sed s'sclone_url:sgit clones'   | awk '$1=="##############"{x=$0;next} $1=="###"{print x, $0; next} 1' | sed  's/^ *//' ;done >$1-$2.txt
+	for page in $(seq 1 20) ; do curl "https://api.github.com/users/$1/$2?page=$page&per_page=99" | grep  'name\|"description\|clone_url' | sed s'\,\\' | sed "/\b\(labels_url\|full_name\)\b/d"  |tr -d '"' |  sed s'jname:j##############j' | sed s'kdescription:k###k'| sed s'sclone_url:sgit clones'   | awk '$1=="##############"{x=$0;next} $1=="###"{print x, $0; next} 1' | sed  's/^ *//' ;done >$1-$2.txt
 	cat $1-$2   | paste - -    | sed 's/############## //g' | sed 's\     ### \,\g' | sed 's\git clone \, git clone \g' > $1-$2.csv
 
 
