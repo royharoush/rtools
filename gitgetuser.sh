b3df73c554ab661cf7dc892682629216feebe3d8
royharoush <roy@haroush.net>
2017-08-02 18:32:32 +0300

    Update gitgetuser.sh

--- gitgetuser.sh
+++ gitgetuser.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 2 ]; then
 	else 
 	echo "Getting the $2 for $1"
 	for page in $(seq 1 20) ; do curl "https://api.github.com/users/$1/$2?page=$page&per_page=99" | grep  'name\|"description\|clone_url' | sed s'\,\\' | sed "/\b\(labels_url\|full_name\)\b/d"  |tr -d '"' |  sed s'jname:j##############j' | sed s'kdescription:k###k'| sed s'sclone_url:sgit clones'   | awk '$1=="##############"{x=$0;next} $1=="###"{print x, $0; next} 1' | sed  's/^ *//' ;done >$1-$2.txt
-	cat $1-$2.txt   | paste - -    | sed 's/############## //g' | sed 's\     ### \,\g' | sed 's\git clone \, git clone \g' > $1-$2.csv
+	cat $1-$2.txt  | tr "," "."  | paste - -    | sed 's/############## //g' | sed 's\     ### \,\g' | sed 's\git clone \, git clone \g' > $1-$2.csv
 
 
 fi
