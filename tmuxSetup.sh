2f3fa627c67f06d8ea8f0f4d0855475660b2e75a
royharoush <roy@haroush.net>
2021-09-25 11:22:34 +0300

    Update tmuxSetup.sh

--- tmuxSetup.sh
+++ tmuxSetup.sh
@@ -1,8 +1,8 @@
 apt-get install tmux git  -y --force-yes --assume-yes
-wget https://raw.githubusercontent.com/royharoush/rtools/master/tmux-31.conf -O /root/.tmux.conf
+wget https://raw.githubusercontent.com/royharoush/rtools/master/tmux-31.conf -O ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/tmux-plugins/tmux-resurrect
 git clone https://github.com/tmux-plugins/tmux-sensible.git ~/tmux-plugins/plugins/tmux-sensible
 git clone https://github.com/tmux-plugins/tmux-continuum.git ~/tmux-plugins//tmux-continuum
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux source-file /root/.tmux.conf
+tmux source-file ~/.tmux.conf
 wget https://gist.githubusercontent.com/royharoush/f4c26a20eb6db711c2fa73a5db89e4b6/raw/31e1a6dec164418d45fd8e52b7d794359d7cae57/bash_completion_tmux.sh -O /etc/bash_completion.d/tmux_completion.sh
